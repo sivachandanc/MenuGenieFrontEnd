4856387381802d549ce36189301038848ccf5ffe
sivachandanc <sivachandan1996@gmail.com>
2025-05-04 20:56:32 -0500

    - Linked Signup process with  superbase - Added a keys in ENV - Added a check to see if password and confirmed passwords are same or not

--- src/components/sign-up/SignUpForm.jsx
+++ src/components/sign-up/SignUpForm.jsx
@@ -1,21 +1,61 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { createClient } from "@supabase/supabase-js";
 
 function SignUpForm() {
+  // Various states
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmedPassword, setConfirmedPassword] = useState("");
+  const [error, setError] = useState("");
+
+  //Getting Superbase URL and Keys from env
+  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+  const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+  // Creating a superbase client
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
+  // Creating a sinup memchanism
+  const signUp = async () => {
+    let { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+    if (error) {
+      console.error("Signup error:", error.message);
+      alert(`Signup failed: ${error.message}`);
+      return;
+    }
+
+    console.log("Signup successful:", data);
+    
+    setError("");
+    alert("Registration successful! Check your email for verification link.");
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (password.length < 6){
+      setError("Length of password should be greater than 6");
+      return;
+    }
+    if (password !== confirmedPassword) {
+      setError("Passwords do not match!");
+      return;
+    }
+    setError("");
+    signUp();
     // You can handle your form submission here
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-[var(--background)] px-4">
       <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md">
-        <h2 className="text-3xl font-bold text-center text-black mb-8">Register</h2>
+        <h2 className="text-3xl font-bold text-center text-black mb-8">
+          Register
+        </h2>
 
         <form onSubmit={handleSubmit} className="space-y-6">
           <div>
@@ -61,6 +101,13 @@ function SignUpForm() {
               placeholder="Confirm your password"
             />
           </div>
+
+          {/* ERROR MESSAGE BLOCK */}
+          {error && (
+            <div className="bg-red-100 text-red-700 px-4 py-2 rounded-md text-center text-sm">
+              {error}
+            </div>
+          )}
 
           <div className="flex items-center space-x-2">
             <input type="checkbox" id="terms" className="h-4 w-4" />
