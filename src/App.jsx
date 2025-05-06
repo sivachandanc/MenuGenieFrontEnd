6831ef4ae326685c16150d16e2b2d737ac37b73d
sivachandanc <sivachandan1996@gmail.com>
2025-05-06 14:35:04 -0500

    Add environment configuration for production and update App component for supabase client usage

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,25 @@ import { createClient } from "@supabase/supabase-js";
 
 function App() {
   //Getting Superbase URL and Keys from env
-    const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-    const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-  
-    // Creating a superbase client
-    const supabaseClient = createClient(supabaseUrl, supabaseKey);
+  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+  const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+  // Creating a superbase client
+  const supabaseClient = createClient(supabaseUrl, supabaseKey);
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-[var(--background)]">
         <NavBar />
         <Routes>
           <Route path="/" element={<HeroSection />} />
-          <Route path="/signup" element={<SignUpForm supabaseClient={supabaseClient}/>}/>
-          <Route path="/login" element={<LoginForm supabaseClient={supabaseClient}/>}/>
+          <Route
+            path="/signup"
+            element={<SignUpForm supabaseClient={supabaseClient} />}
+          />
+          <Route
+            path="/login"
+            element={<LoginForm supabaseClient={supabaseClient} />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
