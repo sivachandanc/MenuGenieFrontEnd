95dab6b00c622870bcc868ce197d3566ca493286
sivachandanc <sivachandan1996@gmail.com>
2025-05-10 20:42:36 -0500

    Refactor App component to use external Supabase client and remove inline client creation

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,12 @@ import HeroSection from "./components/hero-section/HeroSection.jsx";
 import SignUpForm from "./components/sign-up/SignUpForm.jsx";
 import LoginForm from "./components/login/LoginForm.jsx";
 import ChatWindow from "./components/user-chat/ChatWindow.jsx";
-import { createClient } from "@supabase/supabase-js";
 import { useState } from "react";
+import { supabaseClient } from "./supabase-utils/SupaBaseClient.jsx";
 
 function App() {
-  //Getting Superbase URL and Keys from env
-  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-  const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-
   const [chatMode, setChatMode] = useState(false)
-
   // Creating a superbase client
-  const supabaseClient = createClient(supabaseUrl, supabaseKey);
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-[var(--background)]">
